859dbc79fcfe884683dea7034c56473ded51de4e
Hugo Meza <hgmeza114@gmail.com>
2019-10-22 19:14:52 -0400

    Add layout for superheroes view

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Hugo Meza on 10/22/19.
+//  Copyright © 2019 Hugo Meza. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
